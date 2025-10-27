261ed53c1c2b2e8e07bfa5f6cbf8870438b98fb9
rupsh <rupshreekakkar@gmail.com>
2025-10-27 17:26:07 +0530

    - Implement CategoryService with interface and deleteMapping logic. - Created CategoryService class implementing CategoryServiceImp for loose coupling. - Added deleteMapping to delete category by ID using Stream API and return appropriate message.

--- src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
+++ src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
@@ -1,28 +1,37 @@
 package com.ecommerce.sb_ecom.controller;
 
 import com.ecommerce.sb_ecom.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.sb_ecom.service.CategoryService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CategoryController {
-    private List<Category> categories = new ArrayList<>();
+
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories(){
-        return categories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/public/categories")
-    public String createCategory(@RequestBody Category category){
-        categories.add(category);
+    public String createCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
         return "Categories has added successfully!";
+    }
 
+    @DeleteMapping("/api/public/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId){
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
+        }
 
     }
-}
+
---
+++ src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImp.java
@@ -0,0 +1,34 @@
+package com.ecommerce.sb_ecom.service;
+
+import com.ecommerce.sb_ecom.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImp implements CategoryService {
+
+    private List<Category> categories = new ArrayList<>();
+    private long nextId = 1l;
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+            category.setCategoryId(nextId++);
+            categories.add(category);
+    }
+    @Override
+    public String deleteCategory(Long categoryId){
+        Category category = categories.stream()
+                .filter(c->c.getCategoryId().equals(categoryId))
+                .findFirst().orElse(null);
+        if(category == null) return "Category not found";
+        categories.remove(category);
+        return "categories with categoryId: "+categoryId +" deleted successfully.";
+    }
+}
