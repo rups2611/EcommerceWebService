77cb31368a89ab38e7693bb3ad190709f4c1e65c
rupsh <rupshreekakkar@gmail.com>
2025-10-28 22:36:55 +0530

    Implemented updateCategory logic with Optional and exception handling in CategoryService.

--- src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImp.java
+++ src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImp.java
@@ -7,6 +7,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImp implements CategoryService {
@@ -32,5 +33,21 @@ public class CategoryServiceImp implements CategoryService {
                 .findFirst().orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found"));
         categories.remove(category);
         return "categories with categoryId: "+categoryId +" deleted successfully.";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId ){
+    Optional<Category> optionalCategory = categories.stream()
+            .filter(c->c.getCategoryId().equals(categoryId))
+            .findFirst();
+    if(optionalCategory.isPresent()){
+        Category existingCategory = optionalCategory.get();
+        existingCategory.setCategoryName(category.getCategoryName());
+        return existingCategory;
+    }
+    else{
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Category not found");
+    }
+
     }
 }
